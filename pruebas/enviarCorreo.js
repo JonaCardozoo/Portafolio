3b557988e96bece1456760c6eb162c9c37e1ccaa
Jonatan Cardozo <futjona456@gmail.com>
2024-02-16 19:40:39 -0300

    V1

---
+++ pruebas/enviarCorreo.js
@@ -0,0 +1,21 @@
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Enviando...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_cytij2t';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Sent!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
+  
